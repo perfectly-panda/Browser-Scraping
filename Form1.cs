d3f0362efb7193767b2f364b05c7f3bf0ac8c90e
serri588 <samarasoucy@gmail.com>
2016-07-27 20:16:05 -0400

    initial commit
    initial commit

---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+        private WebPage WebPage { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+                WebPage = new WebPage(webBrowser1.DocumentText);
+
+                LinkCount.Text = webBrowser1.Document.Links.Count.ToString();
+                WordCount.Text = WebPage.WordCount().ToString();
+                textBox3.Text = webBrowser1.Document.Body.InnerHtml;
+                textBox2.Text = webBrowser1.Url.ToString();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+            textBox2.Text = "Loading";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(WebPage != null)
+            {
+                textBox3.Text = WebPage.OriginalDocument.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.Document != null)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                var links = webBrowser1.Document.Links;
+
+                foreach (HtmlElement link in links)
+                {
+                    sb.Append(link.GetAttribute("href"));
+                    sb.AppendLine();
+                }
+
+                textBox3.Text = sb.ToString();
+            }
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string sb = webBrowser1.DocumentText;
+
+             var match = "<header(.+)header>";
+
+            var result = Regex.Replace(sb, match, string.Empty, RegexOptions.Singleline);
+
+            webBrowser1.DocumentText = result;
+
+            HtmlAgilityPack.HtmlDocument check = new HtmlAgilityPack.HtmlDocument();
+            check.LoadHtml(webBrowser1.DocumentText);
+
+             var children = check.DocumentNode.Descendants();
+
+            var clean = children.Where(n => n.Name == "header" || n.Name == "footer").ToList();
+            
+            for (int i = clean.Count()-1; i >= 0; i--)
+            {
+                clean[i].Remove();
+            }
+
+            var after = children.Where(n => n.Name == "header" || n.Name == "footer").ToList();
+
+
+            var Injection = true;
+        }
+    }
+
+    public static class ParseHtml
+    {
+       public static int WordCount(System.Windows.Forms.HtmlDocument doc)
+        {
+            int wordCount = 0;
+
+            var elements = doc.Links;
+
+            var check = new Regex("[a-zA-Z0-9]");
+
+            var pattern = @"[\S]+";
+
+            foreach ( HtmlElement element in elements)
+            {
+                if (element.InnerText != null)
+                {
+
+                    var checkResult = Regex.Matches(element.InnerText, pattern).Cast<Match>().Select(m => m.Value).ToList();
+                        //.Where(s => s.Length > 0 && check.IsMatch(s));
+
+                    if(checkResult.Count() > 0)
+                    {
+                        wordCount += checkResult.Count();
+                    }
+                }
+            }
+
+           // var dict = new Dictionary<string, int>();
+
+           // foreach (var word in file)
+            //    if (dict.ContainsKey(word))
+            //        dict[word]++;
+            //    else
+            //        dict[word] = 1;
+
+            return wordCount;
+        }
+    }
+}
---
+++ WebPage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Browser
+{
+    class WebPage
+    {
+        public WebPage(string html)
+        {
+            this.OriginalDocument = new HtmlDocument();
+            this.InjectedDocument = new HtmlDocument();
+            this.OriginalDocument.LoadHtml(html);
+
+            this.InjectedDocument.LoadHtml(html);
+
+            var children = this.InjectedDocument.DocumentNode.Descendants();
+
+            var clean = children.Where(n => n.Name == "header" || n.Name == "footer").ToList();
+
+            for (int i = clean.Count() - 1; i >= 0; i--)
+            {
+                clean[i].Remove();
+            }
+
+
+        }
+
+        public Uri Url { get; set; }
+
+        public HtmlDocument OriginalDocument { get; set; }
+
+        public HtmlDocument InjectedDocument { get; set; }
+
+        public int WordCount()
+        {
+            int result = 0;
+
+            var check = new Regex("[a-zA-Z0-9]");
+
+            var pattern = @"[\S]+";
+
+            var elements = this.InjectedDocument.DocumentNode.ChildNodes.Where(n => n.Name == "html").First().ChildNodes.Where(h => h.Name == "body").First().Descendants();
+
+            foreach (var element in elements.Where(e => e.NodeType == HtmlNodeType.Text))
+            {
+                if (element.InnerText != null)
+                {
+
+                    var checkResult = Regex.Matches(element.InnerText, pattern).Cast<Match>().Select(m => m.Value).ToList();
+                    //.Where(s => s.Length > 0 && check.IsMatch(s));
+
+                    if (checkResult.Count() > 0)
+                    {
+                        result += checkResult.Count();
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
