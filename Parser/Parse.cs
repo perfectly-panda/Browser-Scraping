0e06904959c594265370e4b6271851a5b26bf1c5
serri588 <samarasoucy@gmail.com>
2016-08-23 17:37:07 -0400

    save website
    hooking save back to parser

--- Parser/Parse.cs
+++ Parser/Parse.cs
@@ -8,23 +8,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Core;
+using Service;
 
 namespace Parser
 {
     public class Parse
     {
-        public ParsedWebpage Webpage { get; set; }
+        public ParsedWebpage ParsedWebpage { get; set; }
+
+        private Website Website { get; set; }
+
+        private WebsiteService WebsiteService
+        { get
+            {
+                if (this.WebsiteService == null)
+                    { this.WebsiteService = new WebsiteService(); }
+                return this.WebsiteService;
+            }
+
+            set
+            {
+                this.WebsiteService = value;
+            }
+        }
 
         public async void ParseWebpage(System.Windows.Forms.WebBrowser webBrowser)
         {
-            this.Webpage = new ParsedWebpage(webBrowser);
+            this.ParsedWebpage = new ParsedWebpage(webBrowser);
 
-            await SaveWebpage();
+            this.WebsiteService = new WebsiteService();
+
+            this.Website = await SaveWebsite();
         }
 
-        private Task SaveWebpage()
+        private Task<Website> SaveWebsite()
         {
-            return Task.FromResult(0);
+            var website = new Website();
+
+            website.Domain = this.CleanDomain;
+
+            return WebsiteService.Add(website);
+        }
+
+        private string CleanDomain
+        {
+            get
+            {
+                var domain = this.ParsedWebpage.Domain;
+
+                if (this.ParsedWebpage.SubDomain != "")
+                {
+                    domain = domain.Replace(this.ParsedWebpage.SubDomain + ".", "");
+                }
+
+                return domain;
+            }
         }
     }
 }
